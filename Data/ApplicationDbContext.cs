12c88135df288b0668a8d0d6e3627e4cf66598bc
AlkarimJ1997 <ajiwa97@gmail.com>
2021-10-07 10:48:06 -0700

    added Teams and Players

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Code1st.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,9 +9,20 @@ namespace Code1st.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; } 
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+            
+            // here is where we can seed the tables with sample data
+            modelBuilder.Entity<Team>().HasData(SampleData.GetTeams());
+            modelBuilder.Entity<Player>().HasData(SampleData.GetPlayers());
         }
     }
 }
